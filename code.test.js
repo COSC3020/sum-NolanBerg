bb3d7516102c924ed2d8919e277505e51cd8190c
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-24 16:06:18 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,11 +1,21 @@
+
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js') + '');
 
 const testSum =
-    jsc.forall("array nat", function(arr) {
-        return JSON.stringify(sum(arr)) == JSON.stringify(arr.reduce(function(a, b) { return a + b; }, 0));
+    jsc.forall("array nat", function (arr) {
+        const expectedSum = arr.reduce((a, b) => a + b, 0);
+        const actualSum = sum(arr);
+
+        if (actualSum !== expectedSum) {
+            console.log('Failing case:', arr);
+            console.log('Expected sum:', expectedSum);
+            console.log('Actual sum:', actualSum);
+        }
+
+        return actualSum === expectedSum;
     });
 
 jsc.assert(testSum);
