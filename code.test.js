3a5ec0f3463504638c9cf2e233dd0eb40233615b
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-01 15:16:57 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,5 +1,5 @@
 const jsc = require('jsverify');
-const { sum } = require('./code'); 
+const { sum } = require('./code.js'); 
 
 const testSum = jsc.forall("array nat", function (arr) {
     return sum(arr) === arr.reduce((a, b) => a + b, 0);
